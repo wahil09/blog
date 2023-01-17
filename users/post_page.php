c912afbf7616ecf27022ab142ed9a3f9a8aa4bbc
wahil09 <wahilchettouf@gmail.com>
2023-01-17 21:58:14 +0100

    j'ai codé l'afficharge de la page post_page.php / j'ai précisé l'afficharge du postContent sur l'index page jusqu'à l'index 300 / j'ai régler le lien vers la page sélèctionner

--- users/post_page.php
+++ users/post_page.php
@@ -21,7 +21,7 @@
         $postId = $_GET["id"];
         $post = $postsModel->getPostSelected($postId);
     }
-    echo var_dump($post);
+    
 ?>
 <!DOCTYPE html>
 <html lang="fr-FR">
@@ -30,27 +30,24 @@
     <?php include "header.php" ?>
     <main class="content">
             <div class="container">
-                <?php
-                    
-                    for($i=0; isset($post[$i]); $i++) : ?>
-                        <article class='post'>
-                            <div class='post-image'>
-                                <img src='assets/img/<?php echo $post[$i]['postImage'] ?>' alt=''>
-                            </div>
-                            <div class='post-title'>
-                                <h3><?php echo $post[$i]['postTitle'] ?></h3>
-                            </div>
-                            <div class='post-details'>
-                                <p class='post-info'>
-                                    <span><i class='fa-solid fa-user'></i><?php echo $post[$i]['postAuthor'] ?></span>
-                                    <span><i class='fa-solid fa-calendar-days'></i><?php echo $post[$i]['postDate'] ?></span>
-                                    <span><i class='fa-sharp fa-solid fa-tags'></i><?php echo $post[$i]['postCat'] ?></span>
-                                </p>
-                                <p class='post-description'><?php echo $post[$i]['postContent'] ?></p>
-                                <a href='page_post?<?php echo $post[$i]['id'] ?>' class='btn-custom' >Lire Plus</a>
-                            </div>
-                        </article>
-                    <?php endfor; ?>
+                <section class='posts'>
+                    <article class='post'>
+                        <div class='post-image'>
+                            <img src='assets/img/<?php echo $post['postImage'] ?>' alt=''>
+                        </div>
+                        <div class='post-title'>
+                            <h3><?php echo $post['postTitle'] ?></h3>
+                        </div>
+                        <div class='post-details'>
+                            <p class='post-info'>
+                                <span><i class='fa-solid fa-user'></i><?php echo $post['postAuthor'] ?></span>
+                                <span><i class='fa-solid fa-calendar-days'></i><?php echo $post['postDate'] ?></span>
+                                <span><i class='fa-sharp fa-solid fa-tags'></i><?php echo $post['postCat'] ?></span>
+                            </p>
+                            <p class='post-description'><?php echo $post['postContent'] ?></p>
+                            <a href='page_post?<?php echo $post['id'] ?>' class='btn-custom' >Lire Plus</a>
+                        </div>
+                    </article>
                 </section>
 
                 <div class="sidebar">
