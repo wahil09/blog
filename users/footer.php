1bbb811b1e037e0e86d2992089058a18f9c7b4c6
wahil09 <wahilchettouf@gmail.com>
2023-01-18 11:39:20 +0100

    add extensions to workspaces vscode

--- users/footer.php
+++ users/footer.php
@@ -2,7 +2,7 @@
         <div class="container flex-r">
             <p>&copy; All rights reserved </p>
             <ul class="flex-r">
-                <li><a href="#"><i class="fa-brands fa-github"></i><?php echo $_SESSION['user'] ?>-github</a></li>
+                <li><a href="#"><i class="fa-brands fa-github"></i><?php echo $_SESSION['user'] ?>-GitHub</a></li>
                 <li><a href="#"><i class="fa-brands fa-linkedin"></i><?php echo $_SESSION['user'] ?>-linkedin</a></li>
             </ul>
         </div>
