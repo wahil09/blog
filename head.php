1d77d1d571b7a3df4f0aee1a68b0ab8f5fbbf11a
Wahil-dev <wahil.chettouf@laplateforme.io>
2023-01-24 16:34:58 +0100

    raccourcir le fichier header.php (car il avait des lien commun entre (l'admin, utilisateurs, visiteurs))

--- head.php
+++ head.php
@@ -1,17 +1,22 @@
-
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="wahli chettouf">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@200;300;400;500;600;700;800&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,400;1,500;1,700&display=swap');
+    </style>
 <?php if(isset($_SESSION["role"])) :?>
+    <!-- pour les inscrits -->
+    <link rel="stylesheet" href="../assets/css/all.css">
+    <style>
+        <?php include "../assets/css/style.css" ?>
+        <?php include "../assets/css/MQuiry.css" ?>
+    </style>
+    <title><?php echo $_SESSION['role']?></title>
+
+    <!-- Config de l'admin -->
     <?php if($_SESSION["role"] == "admin") :?>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="author" content="wahli chettouf">
-        <title><?php echo $_SESSION['role']?></title>
-        <link rel="stylesheet" href="../assets/css/all.css">
-        <style>
-            @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@200;300;400;500;600;700;800&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,400;1,500;1,700&display=swap');
-            <?php include "../assets/css/style.css" ?>
-            <?php include "../assets/css/MQuiry.css" ?>
-        </style>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
         <script src="../assets/js/jquery.cookie.js"></script>
@@ -23,32 +28,19 @@
             }
             setScreenHWCookie();
         </script>
+
+    <!-- Config de l'utilisateur -->
     <?php elseif($_SESSION['role'] == "user") :?>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="author" content="wahli chettouf">
-        <title><?php echo $_SESSION['role']?></title>
-        <link rel="stylesheet" href="../assets/css/all.css">
-        <style>
-            @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@200;300;400;500;600;700;800&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,400;1,500;1,700&display=swap');
-            <?php include "../assets/css/style.css" ?>
-            <?php include "../assets/css/MQuiry.css" ?>
-        </style>
+        
     <?php endif; ?>
 
-<?php else :?>
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="author" content="wahli chettouf">
+    <!-- Pour les visiteurs -->
+    <?php else :?>
         <title>index</title>
         <link rel="stylesheet" href="assets/css/all.css">
         <style>
-            @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@200;300;400;500;600;700;800&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,400;1,500;1,700&display=swap');
             <?php include "assets/css/style.css" ?>
             <?php include "assets/css/MQuiry.css" ?>
         </style>
-    </head>
-<?php endif; ?>
+    <?php endif; ?>
+</head>
