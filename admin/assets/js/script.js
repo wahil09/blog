8ce73ed9d7c406dee3d939380a60e79591276596
Wahil-dev <wahil.chettouf@laplateforme.io>
2023-01-29 14:11:11 +0100

    terminer le side-bare (html-css) 80%

--- admin/assets/js/script.js
+++ admin/assets/js/script.js
@@ -3,7 +3,7 @@ const menu = document.querySelector("#menu");
 var bol = true;
 openMenu.addEventListener("click", ()=> {
     if(bol) {
-        menu.style.height = '83px';
+        menu.style.height = '78.5px';
         bol = false;
     } else {
         menu.style.height = '0';
