c85fcf275f0bdd44588c2bd933f4365b15ff3324
Wahil-dev <wahil.chettouf@laplateforme.io>
2023-01-31 11:49:18 +0100

    utiliser la methode getTableName sur toutes les appelles data base au lieu d'écrire chaque fois le nom du table

--- model.php
+++ model.php
@@ -55,9 +55,8 @@
     }
 
     class ModelUsers extends Connection {
-        public $tbname = "users";
         public function userAlreadyRegistered($email, $password) {
-            $request = $this->db->prepare("SELECT * FROM users WHERE password=:password &&email=:email");
+            $request = $this->db->prepare("SELECT * FROM ". $this->getTableName() ." WHERE password=:password &&email=:email");
             $request->bindValue("email", $email);
             $request->bindValue("password", $password);
             $request->execute();
@@ -66,14 +65,14 @@
 
         // ------------ Getters ------------
         function getUsers() {
-            $sth = $this->db->prepare("SELECT * FROM users ");
+            $sth = $this->db->prepare('SELECT * FROM '. $this->getTableName());
             $sth->execute();
             $categories = $sth->fetchAll(PDO::FETCH_ASSOC);
             return $categories;
         }
 
         function getUserDataById($userId) {
-            $request = $this->db->prepare("SELECT * FROM users WHERE id = $userId");
+            $request = $this->db->prepare("SELECT * FROM ". $this->getTableName() ." WHERE id = $userId");
             $request->execute();
             return $request->fetchObject();
         }
@@ -82,7 +81,7 @@
 
         // ------------- Setters ------------- -
         function setUser($username, $email, $password) {
-            $sth = $this->db->prepare("SELECT * FROM users WHERE email = :email");
+            $sth = $this->db->prepare("SELECT * FROM ". $this->getTableName() ." WHERE email = :email");
             $sth->bindValue("email", $email);
             $sth->execute();
             // check if categorie exist
@@ -90,7 +89,7 @@
                 $username = $this->replaceQuote($username);
                 $email = $this->replaceQuote($email);
                 $password = $this->replaceQuote($password);
-                $newUser = "INSERT INTO users(username, email, password, role) VALUES('$username', '$email', '$password', 'user') ";
+                $newUser = "INSERT INTO ". $this->getTableName() ."(username, email, password, role) VALUES('$username', '$email', '$password', 'user') ";
                 $this->db->exec($newUser);
                 // pour afficher un message sur login.php qui dit "categorie ajouter !"
                 $_SESSION['user_inscrit'] = $username;
@@ -105,7 +104,7 @@
 
         // pour modifier le profile
         public function updateProfile($nouveauNom, $nouveauEmail, $nouveauPassword, $nouveauMetier, $nouveauPresentation) {
-            $request = $this->db->prepare("UPDATE users SET username = :nouveauNom, email = :nouveauEmail, password = :nouveauPassword, job = :nouveauMetier, presentation = :nouveauPresentation WHERE id= :idUserCurrent");
+            $request = $this->db->prepare("UPDATE ". $this->getTableName() ." SET username = :nouveauNom, email = :nouveauEmail, password = :nouveauPassword, job = :nouveauMetier, presentation = :nouveauPresentation WHERE id= :idUserCurrent");
             $request->bindValue("nouveauNom", $this->isEmpty($nouveauNom, $_SESSION['login']->username));
             $request->bindValue("nouveauEmail", $this->isEmpty($nouveauEmail, $_SESSION['login']->email));
             $request->bindValue("nouveauPassword", $this->isEmpty($nouveauPassword, $_SESSION['login']->password));
@@ -120,25 +119,25 @@
 
     class ModelCategories extends Connection {
         public function isExist($category_name) {
-            $sth = $this->db->prepare("SELECT * FROM categories WHERE categoryName = :categoryName");
+            $sth = $this->db->prepare("SELECT * FROM ". $this->getTableName() ." WHERE categoryName = :categoryName");
             $sth->bindValue("categoryName", $category_name);
             $sth->execute();
             return empty($sth->fetch());
         }
 
         public function tableIsEmpty() {
-            $sth = $this->db->query("SELECT categoryName FROM categories");
+            $sth = $this->db->query("SELECT categoryName FROM ". $this->getTableName());
             return empty($sth->fetch());
         }
         // ------------ Getters -------------
         function getCategories() {
-            $sth = $this->db->prepare("SELECT id, categoryName FROM categories");
+            $sth = $this->db->prepare("SELECT id, categoryName FROM ". $this->getTableName());
             $sth->execute();
             return $sth->fetchAll(PDO::FETCH_ASSOC);
         }
 
         public function getCategoryId($categoryName) {
-            $sth = $this->db->prepare("SELECT id FROM categories WHERE categoryName= :categoryName");
+            $sth = $this->db->prepare("SELECT id FROM ". $this->getTableName() ." WHERE categoryName= :categoryName");
             $sth->bindValue("categoryName", $categoryName);
             $sth->execute();
             return $sth->fetchObject()->id;
@@ -149,7 +148,7 @@
             // check if categorie exist
             if($this->isExist($category_name)) {
                 $category_name = $this->replaceQuote($category_name);
-                $new_category = "INSERT INTO categories(categoryName, userId) VALUES('$category_name', '$userId')";
+                $new_category = "INSERT INTO ". $this->getTableName() ."(categoryName, userId) VALUES('$category_name', '$userId')";
                 $this->db->exec($new_category);
                 // pour afficher un message sur categories.php qui dit "categorie ajouter !"
                 $_SESSION['category_ajouter'] = $category_name;
@@ -166,7 +165,7 @@
 
     class ModelPosts extends Connection {
         public function isExist($postTitle, $postUserId, $postContent) {
-            $check = $this->db->prepare("SELECT * FROM posts WHERE userId=:userId && postTitle=:postTitle && postContent=:postContent");
+            $check = $this->db->prepare("SELECT * FROM ". $this->getTableName() ." WHERE userId=:userId && postTitle=:postTitle && postContent=:postContent");
             $check->bindValue("postTitle", $postTitle);
             $check->bindValue("postContent", $postContent);
             $check->bindValue("userId", $postUserId);
@@ -175,21 +174,21 @@
         }
 
         public function imageExist($postImage) {
-            $check = $this->db->prepare("SELECT postImage FROM posts WHERE postImage=:postImage");
+            $check = $this->db->prepare("SELECT postImage FROM ". $this->getTableName() ." WHERE postImage=:postImage");
             $check->bindValue("postImage", $postImage);
             $check->execute();
             return !empty($check->fetch());
         }
 
         public function tableIsEmpty() {
-            $sth = $this->db->prepare("SELECT * FROM posts");
+            $sth = $this->db->prepare("SELECT * FROM ". $this->getTableName());
             $sth->execute();
             return !empty($sth);
         }
         
         // ------------ Getters --------------
         function getPosts() {
-            $sth = $this->db->prepare("SELECT * FROM posts");
+            $sth = $this->db->prepare("SELECT * FROM ". $this->getTableName());
             $sth->execute();
             $posts = $sth->fetchAll(PDO::FETCH_ASSOC);
             return $posts;
@@ -197,27 +196,21 @@
 
         public function getPostSelected($postId) {
             if($this->tableIsEmpty()) {
-                $postSelected = $this->db->prepare("SELECT * FROM posts WHERE id= :postID");
+                $postSelected = $this->db->prepare("SELECT * FROM ". $this->getTableName() ." WHERE id= :postID");
                 $postSelected->bindValue("postID", $postId);
                 $postSelected->execute();
-                if(!empty($postSelected)) {
-                    return $postSelected->fetch();
-                }
+                return $postSelected->fetch();
             }
         }
 
-        public function getMyPosts($userId) {
-            $sth = $this->db->prepare("SELECT * FROM posts WHERE id= :userId");
-        }
-
         public function getLastPosts() {
-            $request = $this->db->prepare("SELECT * FROM posts ORDER BY postDate DESC");
+            $request = $this->db->prepare("SELECT * FROM ". $this->getTableName() ." ORDER BY postDate DESC");
             $request->execute();
             return $request->fetchAll(PDO::FETCH_ASSOC);
         }
 
         public function getPostsByCategory($categoryId) {
-            $request = $this->db->prepare("SELECT * FROM posts WHERE categoryId = :categoryId");
+            $request = $this->db->prepare("SELECT * FROM ". $this->getTableName() ." WHERE categoryId = :categoryId");
             $request->bindValue("categoryId", $categoryId);
             $request->execute();
             return $request->fetchAll(PDO::FETCH_ASSOC);
@@ -232,7 +225,7 @@
                 $postImage = $this->replaceQuote($postImage);
                 $postContent = $this->replaceQuote($postContent);
                 $postAuthor = $this->replaceQuote($postAuthor);
-                $newPost = "INSERT INTO posts(postTitle, postCat, postImage, postContent, postAuthor, userId, categoryId) VALUES('$postTitle', '$postCat', '$postImage', '$postContent', '$postAuthor', '$postUserId', '$postCategoryId')";
+                $newPost = "INSERT INTO ". $this->getTableName() ."(postTitle, postCat, postImage, postContent, postAuthor, userId, categoryId) VALUES('$postTitle', '$postCat', '$postImage', '$postContent', '$postAuthor', '$postUserId', '$postCategoryId')";
                 $this->db->exec($newPost);
                 $_SESSION["post-partager"] = true;
             } else {
