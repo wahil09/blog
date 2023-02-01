14f3b3297e311c68ba9ef3e4cd390fa5588e315d
Wahil-dev <wahil.chettouf@laplateforme.io>
2023-02-01 10:45:53 +0100

    coder la page new_post partie backend avec des condition si les champs sont vide

--- admin/posts_manager/new_post.php
+++ admin/posts_manager/new_post.php
@@ -18,42 +18,50 @@
                     session_destroy();
                     header("location:".$BlogPathLien."index.php");
                 }
-            
-                if(isset($_POST["postTitle"], $_POST["Categories"], $_POST["postContent"])) {
-                    include $adminPathInclude."inc/upload_image.php";
-
-                    $postTitle = $_POST["postTitle"];
-                    $postContent = $_POST["postContent"];
-                    $postCat = $_POST["Categories"];
-                    $postAdminId = $_SESSION["login"]->id;
-                    $postAuthor = $_SESSION["login"]->username;
-                    $postImage = $_SESSION["imageName"]; // viens de file upload_image.php
-
-                    if($_SESSION['imageValider']) { // viens de file upload_image.php
-                        $postCategoryId = $categoriesModel->getCategoryId($postCat);
-                        $postsModel->setPost($postTitle, $postCat, $postImage, $postContent, $postAuthor,$postAdminId, $postCategoryId);
-                        if(isset($_SESSION['post-partager'])) {
-                            if($_SESSION["post-partager"]) {
-                                echo "<script>
-                                    alert('Post Partager !');
-                                </script>";
-                                unset($_SESSION["post-partager"]);
-                                header( "refresh: .3; url=".$adminPathLien."index.php" );
-                                exit();
+                
+                if(isset($_POST["save"])) {
+                    if(isset($_POST["postTitle"], $_POST["Categories"], $_POST["postContent"], $_FILES["imageToUpload"])) {
+                        if(!empty($_POST["postTitle"]) && !empty($_POST["Categories"]) && !empty($_POST["postContent"]) && $_FILES["imageToUpload"]["size"] != 0) {
+                            include $adminPathInclude."inc/upload_image.php";
+    
+                            $postTitle = $_POST["postTitle"];
+                            $postContent = $_POST["postContent"];
+                            $postCat = $_POST["Categories"];
+                            $postAdminId = $_SESSION["login"]->id;
+                            $postAuthor = $_SESSION["login"]->username;
+                            $postImage = $_SESSION["imageName"]; // viens de file upload_image.php
+        
+                            if($_SESSION['imageValider']) { // viens de file upload_image.php
+                                $postCategoryId = $categoriesModel->getCategoryId($postCat);
+                                $postsModel->setPost($postTitle, $postCat, $postImage, $postContent, $postAuthor,$postAdminId, $postCategoryId);
+                                if(isset($_SESSION['post-partager'])) {
+                                    if($_SESSION["post-partager"]) {
+                                        echo "<script>
+                                            alert('Post Partager !');
+                                        </script>";
+                                        unset($_SESSION["post-partager"]);
+                                        header( "refresh: .3; url=".$adminPathLien."index.php" );
+                                        exit();
+                                    } else {
+                                        // pour supprimer l'image télécharger 
+                                        unlink($target_file);
+                                        echo "<script>
+                                            alert('Post no partager / Exist déja !');
+                                        </script>";
+                                        unset($_SESSION["post-partager"]);
+                                    }
+                                } 
                             } else {
-                                // pour supprimer l'image télécharger 
-                                unlink($target_file);
                                 echo "<script>
-                                    alert('Post no partager / Exist déja !');
+                                    alert('Post no partager / error-image !');
                                 </script>";
-                                unset($_SESSION["post-partager"]);
+                                unset($_SESSION["imageValider"]);
                             }
-                        } 
-                    } else {
-                        echo "<script>
-                            alert('Post no partager / error-image !');
-                        </script>";
-                        unset($_SESSION["imageValider"]);
+                        } else {
+                            $_SESSION['champ-vide'] = true;
+                            header("location:". $_SERVER["PHP_SELF"]);
+                            exit();
+                        }
                     }
                 }
             }
@@ -81,10 +89,10 @@
 
                         <form enctype="multipart/form-data" method="post" class="flex-c form-new-post">
                             <label for="title">Article name : </label>
-                            <input type="text" name="postTitle" id="title" class="inp-style" required>
+                            <input type="text" name="postTitle" id="title" class="inp-style" >
                             
                             <label for="Categories">Categories : </label>
-                            <select name="Categories" id="Categories" class="inp-style" required>
+                            <select name="Categories" id="Categories" class="inp-style" >
                                 <option value="">Choisir un categorie ...</option>
                                 <?php if(!empty($categories)) :?>
                                     <?php for($i=0; isset($categories[$i]); $i++) :?>
@@ -94,11 +102,18 @@
                             </select>
 
                             <label for="imageToUpload">Article image : </label>
-                            <input type="file" name="imageToUpload" id="imageToUpload" class="inp-style" accept="image/*" required>
+                            <input type="file" name="imageToUpload" id="imageToUpload" class="inp-style" accept="image/*" >
 
-                            <textarea name="postContent" id="post-text" class="inp-style" cols="90" rows="10" required></textarea>
+                            <textarea name="postContent" id="post-text" class="inp-style" cols="90" rows="10" ></textarea>
+                            <?php 
+                                if(isset($_SESSION['champ-vide'])) {
+                                    echo '<p class="error-msg">un champ ou plusieur champs de formulaire sont vide !</p>';
+                                }
+                                unset($_SESSION['champ-vide']);
+                            ?>
+                                
 
-                            <input type="submit" value="upload" class="btn-panel">
+                            <input type="submit" value="save" name="save" class="btn-panel">
                         </form>
                     </section>
 
