2f19e980f743a648e46b06255a6d6a9c55534ae3
Wahil-dev <wahil.chettouf@laplateforme.io>
2023-02-03 22:15:36 +0100

    getsion d'erreur organiser pour la page login

--- login.php
+++ login.php
@@ -2,6 +2,12 @@
     include "inc/config.php";
     include $BlogPathInclude."inc/model.php";
     $usersModel = new ModelUsers();
+    $errors = [
+        "emailError" => "",
+        "passwordError" => "",
+        "identifiantError" => "",
+        "champVide" => "",
+    ];
 
     if(isset($_SESSION["login"])) {
         if($_SESSION["login"]->role == "user") {
@@ -17,20 +23,31 @@
         $email = $_POST["email"];
         $password = $_POST["password"];
         if(!empty($email) && !empty($password)) {
+            // validation email
             if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $userObject = $usersModel->userAlreadyRegistered($email, $password);
-                if($userObject) {
-                    $_SESSION["login"] = $userObject;
+                // validation password : il faut qu'il contient
+                $uppercase = preg_match('@[A-Z]@', $password); // lettre en majuscule
+                $lowercase  = preg_match("@[a-z]@", $password); // lettre en minuscule
+                $number = preg_match("@[0-9]@", $password); // un numéro
+                $specialChars = preg_match("@[^\w]@", $password); // caractère spéciaux 
+                $passLength = strlen($password) >= 8; // 8 caractère au min
+                if($uppercase && $lowercase && $number && $specialChars && $passLength) {
+                    $userObject = $usersModel->userAlreadyRegistered($email, $password);
+                    if($userObject) {
+                        $_SESSION["login"] = $userObject;
+                        header("refresh:0");
+                        exit();
+                    } else {
+                        $errors['identifiantError'] = "votre identifiant n'est pas valide !";
+                    } 
                 } else {
-                    $_SESSION["error_login_user"] = $email;
-                } 
+                    $errors["passwordError"] = "Confirmer que le mote de passe contient :<br> au moins 1 caractère en majuscule, en minuscule, un muméro, caractère spéciaux, 8 caractère au min !";
+                }
             } else {
-                $_SESSION["email-error"] = true;
-                header("refresh:0");
-                exit();
+                $errors['emailError'] = "Entrer une email valide !";
             }
         } else {
-            $_SESSION["champs-vide"] = true;
+            $errors['champVide'] = "un ou plusieurs champs de formulaire sont vide !";
         }
         // on ferme la connexion
         $usersModel->closeConnection();
@@ -60,53 +77,26 @@
                     </a></li>
                     <li><a class="flex-r" href="#"><img src="<?php echo $BlogPathLien?>assets/img/icon-google.png">Google</a></li>
                 </ul>
+
                 <form action="" method="post" class="row-box flex-c">
-                    <!-- afficher un paragraph quand les informations entrer pas valide -->
-                    <?php if(isset($_SESSION['error_login_user'])) {
-                        echo "<p class='error-msg'>votre identifiant n'est pas valide !</p>";
-                        unset($_SESSION['error_login_user']);
-                    } ?>
                     <div class="inp-box">
                         <label for="email">Email</label>
-                        <input class="inp" type="email" name="email" id="email" value="<?php echo isset($_POST['email']) ? $_POST['email'] : ""?>" required>
-                        <?php if(isset($_SESSION["email-error"])) {
-                            echo "<p class='error-msg'>Entrer validate email !</p>";
-                            unset($_SESSION['email-error']);
-                        }?>
+                        <input class="inp" type="text" name="email" id="email" value="<?php echo isset($_POST['email']) ? $_POST['email'] : ""?>" >
                     </div>
                     <div class="inp-box">
                         <label for="password">Password</label><!-- <a href="#">Forgot?</a> -->
-                        <input class="inp" type="password" name="password" id="password" required>
+                        <input class="inp" type="password" name="password" id="password" >
                     </div>
                     <div class="inp-box">
                         <input class="inp" type="submit" value="Sign In" name="submit" id="submit">
                     </div>
+                    <?php echo "<p class='error-msg'>".$errors['emailError']."</p>";?>
+                    <?php echo "<p class='error-msg'>".$errors['identifiantError']."</p>";?>
+                    <?php echo "<p class='error-msg'>".$errors['champVide']."</p>";?>
+                    <?php echo "<p class='error-msg'>".$errors['passwordError']."</p>";?>
                 </form>
                 <p>Not a member? <a href="register.php">Sign up now</a></p>
             </div>
         </section>
-        <div class="error-box" id="errorLoginBox">
-            <div class="row-1 flex-r">
-                <h3>Login failed</h3>
-                <span id="supErrorLoginBox"><i class="fa-solid fa-xmark"></i></span>
-            </div>
-            <hr>
-            <div class="row-2 flex-r">
-                <p>Login failed: formulaire Vide !</p>
-            </div>
-        </div>
-        <?php 
-            if(isset($_SESSION['champs-vide'])) { ?>
-                    <script>
-                        const errorLoginBox = document.querySelector('#errorLoginBox');
-                        errorLoginBox.classList.add('afficher-error-box');
-
-                        const supErrorLoginBox = document.querySelector('#supErrorLoginBox');
-
-                        supErrorLoginBox.addEventListener('click', function() {
-                            errorLoginBox.classList.remove('afficher-error-box');
-                        })
-                    </script>
-            <?php }; unset($_SESSION['champs-vide']); ?>
     </body>
 </html>
